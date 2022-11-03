96be53f73549476fcc456d2cefb65243589ef3d3
Omorhienrhien Efe Destiny <81220100+destiny620@users.noreply.github.com>
2022-11-03 06:21:11 -0700

    Add files via upload

---
+++ 2023 Nigeria election.py
@@ -0,0 +1,13 @@
+your_choice_in_2023_election = input("List of candidates: (peter obi, atiku abubakar, bola tinubu):")
+
+if your_choice_in_2023_election == "peter obi":
+    print("You made the right choice")
+
+elif your_choice_in_2023_election == "atiku abubakar":
+    print("You want to still vote for another Fulani man after eight years of Buhari who is also a Fulani? Think twice!!")
+
+elif your_choice_in_2023_election == "bola tinubu":
+    print("You want to vote for another actogenarian who will spend half of his tenure in UK hospital just like Buhari? Do you hate Nigeria that much? Hmmmmm!!")
+
+else:
+    print("Go and collect your PVC if you have not done so and make sure you vote for the right candidate.")
